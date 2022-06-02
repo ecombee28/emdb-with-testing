00b1459664cab3da374a4ead29ba63d2841b161e
Eric Combee <45884566+ecombee28@users.noreply.github.com>
2022-06-01 21:59:34 -0700

    Updated jest.config.js

--- __tests__/index.test.js
+++
@@ -1,9 +0,0 @@
-import Home from "../pages/index";
-import "@testing-library/jest-dom";
-import { fireEvent, render, screen } from "@testing-library/react";
-
-describe("First test", () => {
-  it("first test", () => {
-    render(<Home />);
-  });
-});
--- jest.config.js
+++ jest.config.js
@@ -10,9 +10,9 @@ const customJestConfig = {
   setupFilesAfterEnv: ["./jest.setup.js"],
   moduleNameMapper: {
     // Handle module aliases (this will be automatically configured for you soon)
-    "^@/components/(.*)$": "<rootDir>/components/$1",
+    "^@/components/(.*)$": "./components/$1",
 
-    "^@/pages/(.*)$": "<rootDir>/pages/$1",
+    "^@/pages/(.*)$": "./pages/$1",
   },
   testEnvironment: "jest-environment-jsdom",
 };
